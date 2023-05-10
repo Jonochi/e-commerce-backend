923026995c21c2360117c7c71c01706fddd88195
Jonochi <tana.jayne@gmail.com>
2023-05-10 14:29:14 -0600

    README added. Fixed FOreignkey bug. Added Product and tag routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,13 +7,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  });
+
+  try {
+    const productData = await Product.findAll({
+      include: [
+        { model: Category },
+        { model: Tag, attributes: ['tag_name'], through: ProductTag, as: 'product_tags' }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        { model: Category },
+        { model: Tag, attributes: ['tag_name'], through: ProductTag, as: 'product_tags' }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+
 });
 
 // create new product
@@ -29,7 +51,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -44,7 +66,7 @@ router.post('/', (req, res) => {
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
 });
 
@@ -94,7 +116,6 @@ router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
   Product.destroy({
     where: {
-
       id: req.params.id,
     },
   })
@@ -102,10 +123,11 @@ router.delete('/:id', (req, res) => {
       if (!product) {
         res.status(404).json({ message: 'No product found with this id' });
         return;
-
       }
-
-
+      res.json({ message: "Product Deleted!"});
+    })
+    .catch((err) => {
+      res.status(400).json(err);
     });
 
 });
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# E-commerce Back End Starter Code
+# E-Commerce Backend
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Description
+
+This project is Module 13 challenge of U of U's professional bootcamp. This project uses Insomnia as an interface to traverse and manipulate databases. The databases are created using the Sequelize package. The project is a backend for a potential business to store data about products for sale including stock quantity, price, and category. Each category may have many different products, and each tag may describe many different products.
+## Installation
+
+N/A
+
+## Usage
+   
+
+## Demo
+
+[Demo video here](https://drive.google.com/file/d/1A6oofS_gZvdMi59WEq-mX8UFtBNqPMjI/view)
+   
+
+## Credits
+
+N/A
+## License
+
+N/A
